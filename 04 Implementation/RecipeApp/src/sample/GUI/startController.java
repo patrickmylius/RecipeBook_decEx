6601620c42990e027cf2115cb5ee82afd8d0f167
patrickmylius <patrickmylius@outlook.dk>
2020-12-22 12:11:10 +0100

    Finished text holders in showRecipeBurger.fxml

--- 04 Implementation/RecipeApp/src/sample/GUI/startController.java
+++ 04 Implementation/RecipeApp/src/sample/GUI/startController.java
@@ -3,6 +3,7 @@ package sample.GUI;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
