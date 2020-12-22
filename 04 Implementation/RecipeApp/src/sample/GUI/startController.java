645dba3180e4744cbfec2a5b606cf18a71d7153c
patrickmylius <patrickmylius@outlook.dk>
2020-12-22 11:01:36 +0100

    Implemented 3 popup windows, to hold each recipe.
    showRecipeBurger.fxml
    showRecipePizza.fxml
    showRecipeTomatoSoup.fxml

--- 04 Implementation/RecipeApp/src/sample/GUI/startController.java
+++ 04 Implementation/RecipeApp/src/sample/GUI/startController.java
@@ -1,17 +1,38 @@
 package sample.GUI;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class startController {
+    @FXML
+    private ChoiceBox recipe;
 
     public void handleRecipes(ActionEvent event) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("recipes.fxml"));
-        Stage showRecipe = fxmlLoader.load();
-        showRecipe.initModality(Modality.APPLICATION_MODAL); //Locks primaryStage
-        showRecipe.show();
+        String recipeChoice = (String) recipe.getValue();
+        switch (recipeChoice.toLowerCase()) {
+            case "burger":
+                FXMLLoader fxmlLoaderBurger = new FXMLLoader(getClass().getResource("showRecipeBurger.fxml"));
+                Stage showBurger = fxmlLoaderBurger.load();
+                showBurger.initModality(Modality.APPLICATION_MODAL); //Locks primaryStage
+                showBurger.show();
+                break;
+            case "pizza":
+                FXMLLoader fxmlLoaderPizza = new FXMLLoader(getClass().getResource("showRecipePizza.fxml"));
+                Stage showPizza = fxmlLoaderPizza.load();
+                showPizza.initModality(Modality.APPLICATION_MODAL); //Locks primaryStage
+                showPizza.show();
+                break;
+            case "tomato soup":
+                FXMLLoader fxmlLoaderTomatoSoup = new FXMLLoader(getClass().getResource("showRecipeTomatoSoup.fxml"));
+                Stage showSoup = fxmlLoaderTomatoSoup.load();
+                showSoup.initModality(Modality.APPLICATION_MODAL); //Locks primaryStage
+                showSoup.show();
+
+        }
 
     }
 }
